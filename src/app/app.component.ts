1bfa0c0da1ebb55d0f7ecd0e5331a95a9d1cc02e
DN <asmrdnsingh@gmail.com>
2022-05-13 13:06:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'counter-app';
+
+  countNumber = 0;
+
+  
+  resetBtn()
+  {
+    this.countNumber = 0;
+  }
+
+
+  increaseBtn()
+  {
+    this.countNumber = this.countNumber + 1;
+  }
+
+
+  decreaseBtn()
+  {
+    this.countNumber = this.countNumber - 1;
+    if(this.countNumber<0)
+    {
+      this.countNumber = 0;
+    }
+  }
 }
